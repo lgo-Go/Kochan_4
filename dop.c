87c73682698bdd8fd820ac78c0599ca80b5fa1fe
Игорь <pupokbomzha@yandex.ru>
2018-03-20 11:29:50 +0300

    +dop.c

---
+++ dop.c
@@ -0,0 +1,10 @@
+/*Выдайт диапазоны значений некоторых типов*/
+#include <stdio.h>
+int main()
+{
+	printf ("char                 -128 to 127\nunsigned char           0 to 255\n");
+	printf ("short             -32,768 to 32,767\nunsigned short          0 to 65,535\n");
+	printf ("int               -2.1e+9 to 2.1t+9\nunsigned int            0 to 4.2e+9\n");
+	printf ("long              -2.1e+9 to 2.1t+9\nunsigned long           0 to 4.2e+9\n");
+	return 0;
+}
